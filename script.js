830219e4be3de23e69af980666a329c5c1adb8ad
iron-frog <124222884+iron-frog@users.noreply.github.com>
2023-03-28 23:30:04 +0100

    Add UI components
    Allows the user change colour of their drawings and each line chan
    be a different colour. Also changed the input of squares to a slider.

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const container = document.querySelector(".container");
 const button = document.querySelector(".but");
+const slider = document.querySelector("#squares");
+const value = document.querySelector("#value");
+const colorVal = document.querySelector("#col");
+value.textContent = slider.value +" * " + slider.value ;
+//
 
-const containerWidth = container.offsetWidth-20;
-const containerHeight = container.offsetHeight-20;
 
-function createGrid(num){
-    while(container.firstChild){
+const containerWidth = container.offsetWidth - 20;
+const containerHeight = container.offsetHeight - 20;
+let startColour = [];
+let isDrawing = false;
+
+
+function createGrid(num) {
+    while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
     const time = new Date();
     const seconds = time.getSeconds();
-    const squareWidth = (containerWidth/num) + "px";
-    const squareHeight = (containerHeight/num) + "px";
+    const squareWidth = (containerWidth / num) + "px";
+    const squareHeight = (containerHeight / num) + "px";
 
-    for(let i =0; i<num*num; i++){
+    for (let i = 0; i < num * num; i++) {
         let square = document.createElement("div");
         square.className = "square";
         square.style.width = squareWidth;
@@ -21,40 +30,42 @@ function createGrid(num){
         container.appendChild(square);
     }
     const newTime = new Date();
-    const newSeconds =newTime.getSeconds();
-    console.log("Time taken: " + (newSeconds-seconds));
+    const newSeconds = newTime.getSeconds();
+    console.log("Time taken: " + (newSeconds - seconds));
 }
 
-
-function howMany(){
-    let amount = parseInt(prompt("Input for how many squares per side"));
-    return amount
-}
-
-button.addEventListener("click", ()=>{
-    let amount = howMany();
-    if(!Number.isInteger(amount)){
-        alert("Please enter a number");
-    }
-    else if(amount >100){
-        alert("Please enter number less than 100");
-    }
-    else{
-        createGrid(amount);
-    }
-});
-
-function draw(e){
-    if(!(e.target.className =="square")){
+function draw(e) {
+    if (!(e.target.className == "square")) {
         return;
     }
-    const square = document.querySelectorAll(".square");
-    square.forEach(item => item.addEventListener('mousemove', ()=>{
-        item.classList.add("hover");
-    }))
-    // square.forEach(item=> item.addEventListener('transitionend', ()=>{
-    //         item.classList.remove("hover");
-    //     }));
-}
 
+    const square = document.querySelectorAll(".square");
+    square.forEach(item => item.addEventListener('mousemove', () => {
+        
+        if (isDrawing) {
+            item.classList.add("hover");
+            item.style.backgroundColor = colorVal.value;
+        }
+        
+    }));
+    square.forEach(item => item.addEventListener('mousedown', () => {
+        isDrawing = true;
+    }));
+    square.forEach(item => item.addEventListener('mouseup', () => {
+        isDrawing = false;
+    }));
+    // square.forEach(item => item.addEventListener('mouseout', () => {
+    //     isDrawing = false;
+    // }));
+
+}
+createGrid(slider.value);
+slider.addEventListener('input', (e)=>{
+    value.textContent = slider.value +" * " + slider.value ;
+    createGrid(e.target.value);
+})
+
+container.addEventListener('mousemout', ()=> isDrawing =false);
 container.addEventListener('mousemove', draw);
+
+
